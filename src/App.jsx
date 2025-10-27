2739b3d7a10c9158a2ed790d99047a3c7ac9887e
MorinoCode <mortezamastercode@gmail.com>
2025-10-27 14:16:41 +0100

    add signop page to routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+
+// import pages
 import Navbar from "./component/navbar/Navbar";
+import Signup from "./pages/signup/Signup";
 
 function App() {
   function PrivateRoute({ children }) {
@@ -21,6 +24,7 @@ function App() {
         <Navbar />
 
         <Routes>
+          <Route path="/signup" element={<Signup />} />
 
           
         </Routes>
